c022b557cf843a653ff3b339901079f993f250d1
jan.de.deurwaerder <jan.de.deurwaerder@HOW-94NWFS3>
2023-06-08 14:21:23 +0200

    Add project files.

---
+++ Scala.Adovb5.Core/Services/KachelService.cs
@@ -0,0 +1,138 @@
+﻿using Scala.Adovb5.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scala.Adovb5.Core.Services
+{
+    public class KachelService
+    {
+        public List<Soort> GetSoorten()
+        {
+            string sql;
+            sql = "select * from soorten order by soortnaam";
+            List<Soort> soorten = new List<Soort>();
+            DataTable dt = DBServices.ExecuteSelect(sql);
+            foreach (DataRow dr in dt.Rows)
+            {
+                string id = dr["id"].ToString();
+                string soortnaam = dr["soortnaam"].ToString();
+                soorten.Add(new Soort(id, soortnaam));
+            }
+            return soorten;
+        }
+        private bool SoortInUse(Soort soort)
+        {
+            string sql;
+            sql = "select count(*) from kachels ";
+            sql += $" where soortid = '{soort.Id}'";
+            string strAantal = DBServices.ExecuteScalar(sql);
+            if (int.Parse(strAantal) > 0)
+                return true;
+            else
+                return false;
+        }
+        private bool SoortnaamExists(Soort soort)
+        {
+            string sql;
+            sql = "select count(*) from soorten ";
+            sql += $" where Id <> '{soort.Id}' ";
+            sql += $" and soortnaam = '{Helper.HandleQuotes(soort.Soortnaam)}' ";
+            string strAantal = DBServices.ExecuteScalar(sql);
+            if (int.Parse(strAantal) > 0)
+                return true;
+            else
+                return false;
+        }
+        public bool SoortToevoegen(Soort soort)
+        {
+            if (SoortnaamExists(soort))
+            {
+                throw new Exception("Soortnaam reeds in gebruik");
+            }
+            string sql;
+            sql = "insert into soorten (id, soortnaam) ";
+            sql += " values (";
+            sql += $" '{soort.Id}' , ";
+            sql += $" '{Helper.HandleQuotes(soort.Soortnaam)}' )";
+            return DBServices.ExecuteCommand(sql);
+        }
+        public bool SoortWijzigen(Soort soort)
+        {
+            if (SoortnaamExists(soort))
+            {
+                throw new Exception("Soortnaam reeds in gebruik");
+            }
+            string sql;
+            sql = "update soorten set ";
+            sql += $" soortnaam = '{Helper.HandleQuotes(soort.Soortnaam)}' ";
+            sql += $" where id = '{soort.Id}' ";
+            return DBServices.ExecuteCommand(sql);
+        }
+        public bool SoortVerwijderen(Soort soort)
+        {
+            if (SoortInUse(soort))
+            {
+                throw new Exception("Soort nog in gebruik");
+            }
+            string sql;
+            sql = "delete from soorten ";
+            sql += $" where id = '{soort.Id}' ";
+            return DBServices.ExecuteCommand(sql);
+        }
+
+        public List<Kachel> GetKachels(Soort soortFilter = null)
+        {
+            string sql;
+            sql = "select * from kachels ";
+            if (soortFilter != null)
+            {
+                sql += $" where soortid = '{soortFilter.Id}' ";
+            }
+            sql += " order by merk, serie";
+            List<Kachel> kachels = new List<Kachel>();
+            DataTable dt = DBServices.ExecuteSelect(sql);
+            foreach (DataRow dr in dt.Rows)
+            {
+                string id = dr["id"].ToString();
+                string soortId = dr["soortid"].ToString();
+                string merk = dr["merk"].ToString();
+                string serie = dr["serie"].ToString();
+                decimal prijs = decimal.Parse(dr["prijs"].ToString());
+                kachels.Add(new Kachel(id, soortId, merk, serie, prijs));
+            }
+            return kachels;
+        }
+        public bool KachelToevoegen(Kachel kachel)
+        {
+            string sql;
+            sql = "insert into kachels (id, soortid, merk, serie, prijs) ";
+            sql += " values (";
+            sql += $" '{kachel.Id}' , ";
+            sql += $" '{kachel.SoortId}' , ";
+            sql += $" '{Helper.HandleQuotes(kachel.Merk)}' , ";
+            sql += $" '{Helper.HandleQuotes(kachel.Serie)}' , ";
+            sql += $" {kachel.Prijs} )";
+            return DBServices.ExecuteCommand(sql);
+        }
+        public bool KachelWijzigen(Kachel kachel)
+        {
+            string sql;
+            sql = "update kachels set ";
+            sql += $" soortid = '{kachel.SoortId}' , ";
+            sql += $" merk = '{Helper.HandleQuotes(kachel.Merk)}' , ";
+            sql += $" serie = '{Helper.HandleQuotes(kachel.Serie)}' , ";
+            sql += $" prijs = {kachel.Prijs} ";
+            return DBServices.ExecuteCommand(sql);
+        }
+        public bool KachelVerwijderen(Kachel kachel)
+        {
+            string sql = "delete from kachels ";
+            sql += $" where id = '{kachel.Id}' ";
+            return DBServices.ExecuteCommand(sql);
+        }
+    }
+}
---
+++ Scala.Adovb5.Wpf/WinSoorten.xaml.cs
@@ -0,0 +1,184 @@
+﻿using Scala.Adovb5.Core.Entities;
+using Scala.Adovb5.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Scala.Adovb5.Core.Entities;
+using Scala.Adovb5.Core.Services;
+
+namespace Scala.Adovb5.Wpf
+{
+    /// <summary>
+    /// Interaction logic for WinSoorten.xaml
+    /// </summary>
+    public partial class WinSoorten : Window
+    {
+        public bool refreshen = false;
+        KachelService kachelService = new KachelService();
+        bool isNew;
+        public WinSoorten()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LinksActief();
+            VulListbox();
+        }
+
+        private void LstSoorten_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ClearControls();
+            if (lstSoorten.SelectedItem != null)
+            {
+                Soort soort = (Soort)lstSoorten.SelectedItem;
+                txtSoortnaam.Text = soort.Soortnaam;
+            }
+        }
+
+        private void BtnNieuw_Click(object sender, RoutedEventArgs e)
+        {
+            isNew = true;
+            RechtsActief();
+            ClearControls();
+        }
+
+        private void BtnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstSoorten.SelectedItem != null)
+            {
+                isNew = false;
+                RechtsActief();
+            }
+        }
+
+        private void BtnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstSoorten.SelectedItem != null)
+            {
+                if (MessageBox.Show("Ben je zeker?", "Toestel verwijderen", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    Soort soort = (Soort)lstSoorten.SelectedItem;
+                    bool geslaagd = true;
+                    try
+                    {
+                        geslaagd = kachelService.SoortVerwijderen(soort);
+                    }
+                    catch(Exception fout)
+                    {
+                        MessageBox.Show(fout.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                    }
+                    if (!geslaagd)
+                    {
+                        MessageBox.Show("DB ERROR", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                    ClearControls();
+                    VulListbox();
+                    refreshen = true;
+                }
+            }
+        }
+
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+            string soortnaam = txtSoortnaam.Text.Trim();
+            if (soortnaam.Length == 0)
+            {
+                MessageBox.Show("soortnaam invoeren", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtSoortnaam.Focus();
+                return;
+            }
+            Soort soort;
+            if (isNew)
+            {
+                soort = new Soort(soortnaam);
+                bool geslaagd = true;
+                try
+                {
+                    kachelService.SoortToevoegen(soort);
+                }
+                catch(Exception fout)
+                {
+                    MessageBox.Show(fout.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (!geslaagd)
+                {
+                    MessageBox.Show("DB ERROR", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            else
+            {
+                soort = (Soort)lstSoorten.SelectedItem;
+                soort.Soortnaam = soortnaam;
+                bool geslaagd = true;
+                try
+                {
+                    geslaagd = kachelService.SoortWijzigen(soort);
+                }
+                catch(Exception fout)
+                {
+                    MessageBox.Show(fout.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+
+                }
+                if (!geslaagd)
+                {
+                    MessageBox.Show("DB ERROR", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            VulListbox();
+            LinksActief();
+            lstSoorten.SelectedValue = soort.Id;
+            LstSoorten_SelectionChanged(null, null);
+            refreshen = true;
+
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+            ClearControls();
+            LinksActief();
+            LstSoorten_SelectionChanged(null, null);
+        }
+
+        private void LinksActief()
+        {
+            grpSoorten.IsEnabled = true;
+            grpDetails.IsEnabled = false;
+            btnBewaren.Visibility = Visibility.Hidden;
+            btnAnnuleren.Visibility = Visibility.Hidden;
+        }
+        private void RechtsActief()
+        {
+            grpSoorten.IsEnabled = false;
+            grpDetails.IsEnabled = true;
+            btnBewaren.Visibility = Visibility.Visible;
+            btnAnnuleren.Visibility = Visibility.Visible;
+        }
+        private void VulListbox()
+        {
+            lstSoorten.ItemsSource = kachelService.GetSoorten();
+            lstSoorten.Items.Refresh();
+        }
+        private void ClearControls()
+        {
+            txtSoortnaam.Text = "";
+        }
+    }
+}
